3f213057699843f311264c53587f6f5fac4aca44
Shashi Kumar <skdhanjal777@gmail.com>
2016-01-20 14:45:35 +0530

    initial comments

--- js/productsData.js
+++ js/productsData.js
@@ -1,38 +1,37 @@
-
 // can't make ajax call on local file system so exposing data through object
 
 var productsDataService = {},
-	DisplayProductsModule = DisplayProductsModule || {};
+    DisplayProductsModule = DisplayProductsModule || {};
 
-productsDataService = (function(){
-	var productsData  = [];
-
-	// private function	
-	function getData (){
-		return productsData;
-	}
-
-	function loadJSON() {   
-	    var ajaxXHRObject = new XMLHttpRequest();
-
-	    ajaxXHRObject.overrideMimeType("application/json");
-	    ajaxXHRObject.open('GET', 'productsData.json', true); 
-
-	    ajaxXHRObject.onreadystatechange = function () {
-	          if (ajaxXHRObject.readyState == 4 && ajaxXHRObject.status == "200") {
-		          if(ajaxXHRObject.responseText){
-		          	productsData = JSON.parse(ajaxXHRObject.responseText)["products"];
-		          	DisplayProductsModule.initProducts(productsData, 'productDispalySection');
-		          }
-	          }
-	    };
-	    ajaxXHRObject.send(null);  
-	 }
-
-	 function init(){
-	 	loadJSON();
-	 }
-	
-	 init();
-
+productsDataService = (function () {
+    var productsData = [];
+    
+    // private function	
+    function getData() {
+        return productsData;
+    }
+    
+    function loadJSON() {
+        var ajaxXHRObject = new XMLHttpRequest();
+        
+        ajaxXHRObject.overrideMimeType("application/json");
+        ajaxXHRObject.open('GET', 'productsData.json', true);
+        
+        ajaxXHRObject.onreadystatechange = function () {
+            if (ajaxXHRObject.readyState == 4 && ajaxXHRObject.status == "200") {
+                if (ajaxXHRObject.responseText) {
+                    productsData = JSON.parse(ajaxXHRObject.responseText)["products"];
+                    DisplayProductsModule.initProducts(productsData, 'productDispalySection');
+                }
+            }
+        };
+        ajaxXHRObject.send(null);
+    }
+    
+    function init() {
+        loadJSON();
+    }
+    
+    init();
+    
 })();
